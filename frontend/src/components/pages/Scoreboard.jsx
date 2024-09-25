a72c3481c6b666b1514f2f18261d81255a474ca9
Liam Vermette <Liam.vermette@gmail.com>
2024-09-24 22:39:01 -0400

    styled scoreboard

--- frontend/src/components/pages/Scoreboard.jsx
+++ frontend/src/components/pages/Scoreboard.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext"; // Adjust the path as needed
+import scoreBack from "../../assets/videos/ScoreboardBack.gif";
 
 const Scoreboard = () => {
   const { fishScores, surfScores } = useContext(AuthContext); // Use useContext to access AuthContext
@@ -7,32 +8,37 @@ const Scoreboard = () => {
   console.log("Rendering Scoreboard", { fishScores, surfScores });
 
   return (
-    <div>
-      <h2>Fish Scores</h2>
-      <ul>
-        {fishScores.length > 0 ? (
-          fishScores.map((data) => (
-            <li key={data._id}>
-              {data.user.username}: {data.fishWeight} lbs {/* Display username and fish weight */}
-            </li>
-          ))
-        ) : (
-          <li>No fish scores available</li>
-        )}
-      </ul>
+    <div className="scoreboard">
+      <img src={scoreBack} alt="Fish Animation" className="scoreBack" />
+      <div className="zpop4">
+        <h2>Fish Scores</h2>
+        <ul>
+          {fishScores.length > 0 ? (
+            fishScores.map((data) => (
+              <li key={data._id}>
+                {data.user.username}: {data.fishWeight} lbs{" "}
+                {/* Display username and fish weight */}
+              </li>
+            ))
+          ) : (
+            <li>No fish scores available</li>
+          )}
+        </ul>
 
-      <h2>Surf Scores</h2>
-      <ul>
-        {surfScores.length > 0 ? (
-          surfScores.map((data) => (
-            <li key={data._id}>
-              {data.user.username}: {data.stylePoints} style points {/* Display username and style points */}
-            </li>
-          ))
-        ) : (
-          <li>No surf scores available</li>
-        )}
-      </ul>
+        <h2>Surf Scores</h2>
+        <ul>
+          {surfScores.length > 0 ? (
+            surfScores.map((data) => (
+              <li key={data._id}>
+                {data.user.username}: {data.stylePoints} style points{" "}
+                {/* Display username and style points */}
+              </li>
+            ))
+          ) : (
+            <li>No surf scores available</li>
+          )}
+        </ul>
+      </div>
     </div>
   );
 };
